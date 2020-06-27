b216bef7795ae27151ba9382466e8ef4900076e1
leyuri <leyuri97@gmail.com>
2020-06-27 15:38:32 +0900

    Add renderRow function Create functions to reduce repetition

--- src/components/board.js
+++ src/components/board.js
@@ -2,27 +2,24 @@ import React from 'react';
 import Square from './square';
 
 class Board extends React.Component {
-    render() {  //JSX
-        const status = "Next player: X";
-        return(
-        <div>
-            <div className="status">{status}</div>
+
+    renderRow() {
+        return (
             <div>
                 <Square value="1"/>
                 <Square value="2"/>
                 <Square value="3"/>
             </div>
-            <div>
-                <Square  value="4"/>
-                <Square  value="5"/>
-                <Square  value="6"/>
-            </div>
-            <div>
-                <Square  value="7"/>
-                <Square  value="8"/>
-                <Square  value="9"/>
-            </div>
-
+        )
+    }
+    render() {  //JSX
+        const status = "Next player: X";
+        return(
+        <div>
+            <div className="status">{status}</div>
+            {this.renderRow()}
+            {this.renderRow()}
+            {this.renderRow()}
         </div>
         )
     }
