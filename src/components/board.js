8a60717ebc2e96fa0d8ec65a512371670950bfbb
leyuri <leyuri97@gmail.com>
2020-06-27 15:32:28 +0900

    Add received props value

--- src/components/board.js
+++ src/components/board.js
@@ -3,23 +3,24 @@ import Square from './square';
 
 class Board extends React.Component {
     render() {  //JSX
+        const status = "Next player: X";
         return(
         <div>
-            <div className="status">Next player: X</div>
+            <div className="status">{status}</div>
             <div>
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value="1"/>
+                <Square value="2"/>
+                <Square value="3"/>
             </div>
             <div>
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square  value="4"/>
+                <Square  value="5"/>
+                <Square  value="6"/>
             </div>
             <div>
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square  value="7"/>
+                <Square  value="8"/>
+                <Square  value="9"/>
             </div>
 
         </div>
